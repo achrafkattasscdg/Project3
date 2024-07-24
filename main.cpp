70f187f2ee583de12e8e6660cfd0248ef346691d
achrafkattasscdg <achraf.kattas@cdgcapital.ma>
2024-07-24 14:41:04 +0100

    #

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "test before pull" << std::endl;
-    std::cout << "test after pull" << std::endl;
+
     return 0;
 }
